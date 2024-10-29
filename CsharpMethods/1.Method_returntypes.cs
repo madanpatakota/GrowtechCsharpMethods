434f42075492baf7d9e0d4d77385f7d2a403a5d5
Madn <madan.patakota@gmail.com>
2024-10-29 20:32:38 +0530

    Add project files.

---
+++ CsharpMethods/1.Method_returntypes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpMethods
+{
+    class Method_returntype
+    {
+        //void method - Non return type methods
+
+        // understand diff b/w defination and calling 
+
+        // always main method executes . no execuses
+        public static void Main()
+        {
+
+            //double piVlue = 3.17;
+            //if(piVlue == 3.17)
+            //{
+            //}
+            //else
+            //{
+
+            //}
+           string status = Method_returntype.getEmpStatus(); // 3+ Exp
+           Console.WriteLine(status);
+
+            Method_returntype.PrintMessage();
+
+            Console.ReadLine();
+
+
+
+        }
+        public static void PrintMessage()
+        {
+            
+            Console.WriteLine("Calling PrintMessage Method");
+        }
+        public static void PrintCalculateAddtion()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("Addition is a and b is I.e.", a + b);
+        }
+        public static int getTotalValue()
+        {
+            int a = 10;
+            int b = 20;
+            return a + b;
+        }
+        //Always think what is the input and what is the return type
+        public static int[] getSomeNumbers()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            int a = 10;
+
+            return numbers;
+        }
+        public static string getEmpStatus()
+        {
+            string studentName = "Siva";
+            
+            if (studentName == "Keerthi")
+            {
+                return "Fresher";
+            }
+            else
+            {
+                return "3+ Exp";
+            }
+        }
+
+
+
+    }
+}
---
+++ CsharpMethods/2.Methods_inputs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpMethods
+{
+    //void method - Non return type methods
+    // understand diff b/w defination and calling 
+    // always main method executes . no execuses
+    //Always think what is the input and what is the return type
+    class Methods_Inputs
+    {
+        public static void Main()
+        {
+            //Methods_Inputs.Student1Details("Madan" , "C#.net");
+
+            //Methods_Inputs.Student3Details("Madan" , "Angular");
+
+            //i want to pass multiple arguments
+            Methods_Inputs.SivaDetails("Siva", "Angular" , "He is from TamilNadu" ,"He is in Bangolore");
+
+            Console.ReadLine();
+        }
+
+        //Parameters
+        static void KeerthiDetails(string name , string CourseName)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(CourseName);
+        }
+
+        static void ShashankDetails(string name, string CourseName)
+        {
+            //$"{}"
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {CourseName}");
+        }
+
+        static void KarthikDetails(string name, string CourseName = "No Course")
+        {
+            //$"{}"
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"CourseName: {CourseName}");
+        }
+
+        static void SivaDetails(string name,string CourseName, params string[] details)
+        {
+            //
+            Console.WriteLine(name);
+            Console.WriteLine(CourseName);
+
+            for(int index =0; index< details.Length;index = index + 1)
+            {
+                Console.WriteLine(details[index]);
+            }
+            
+
+        }
+
+
+        //All the methods i have taken the void
+
+        //Home work : Prepare non-void methods 
+
+    }
+}
